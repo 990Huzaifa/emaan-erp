9bb367d83b4e6865ae8d08b702580d64dda5d7b7
990Huzaifa <syedhuzaifaali366@gmail.com>
2024-09-13 07:52:07 +0500

    changes from my laptop

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Exception;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Database\QueryException;
+use Illuminate\Support\Facades\Validator;
+
+class AuthController extends Controller
+{
+    public function login(Request $request):JsonResponse{
+        try{
+            $validator = Validator::make(
+                $request->all(),[
+                    'email'=>'required|string|email',
+    
+            ],[
+                'name.required'=>'Name is Required',
+                'name.string'=>'Name is must be a string',
+    
+                'email.required' => 'Email is required.',
+                'email.email' => 'Please provide a valid email address.',
+    
+                'password.required' => 'Password is required.',
+                'password.string' => 'Password must be a string.',
+            ]);
+            if ($validator->fails()) throw new Exception($validator->errors()->first(), 400);
+            $user = User::where('email',$request->email)->first();
+            if (empty($user) || !Hash::check($request->password, $user->password)) throw new Exception('Invalid login credentials.', 404);
+            $accessToken = $user->createToken('authToken')->plainTextToken;
+            // $permissionNames  = $user->getAllPermissions();
+            $permissionNames = $user->getAllPermissions()->pluck('name');
+            // $permission=['user'=>$permissionNames];
+            return response()->json([$user,$accessToken, $permissionNames]);
+        }catch(QueryException $e){
+            return response()->json(['DB error' => $e->getMessage()], $e->getCode() ?:400);
+
+        }catch(Exception $e){
+            return response()->json(['error' => $e->getMessage()], $e->getCode() ?: 400);
+        }
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\RoleController;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\PermissionController;
@@ -20,10 +21,12 @@ use App\Http\Controllers\PermissionController;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+    
 });
 
+Route::post('login',[AuthController::class,'login']);
 
-Route::apiResource('user',UserController::class)->only('index','show','store');
+Route::apiResource('user',UserController::class)->only('index','store','show','update');
 
 Route::apiResource('permission',PermissionController::class)->only('index','store');
 // Route::apiResource('role',RoleController::class)->only('index','store');
