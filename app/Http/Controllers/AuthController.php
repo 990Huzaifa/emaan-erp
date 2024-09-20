f8b70598a0d7a40b0f369b2f800bdf518ee5e2f3
990Huzaifa <syedhuzaifaali366@gmail.com>
2024-09-20 20:43:32 +0500

    changes

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Admin;
 use Exception;
 use App\Models\User;
 use Illuminate\Http\Request;
@@ -18,6 +19,7 @@ class AuthController extends Controller
                 $request->all(),[
                     'email'=>'required|string|email',
                     'password'=>'required|string',
+                    'type'=> 'required|string|in:admin,user',
     
             ],[
                 'name.required'=>'Name is Required',
@@ -28,22 +30,33 @@ class AuthController extends Controller
     
                 'password.required' => 'Password is required.',
                 'password.string' => 'Password must be a string.',
+
+                'type.required' => 'Login type is required.',
+                'type.string' => 'Login type must be a string.',
+                'type.in' => 'Login type Invalid.',
             ]);
             if ($validator->fails()) throw new Exception($validator->errors()->first(), 400);
-            
+            if($request->type == 'user'){
                 $user = User::where('email',$request->email)->first();
                 if (empty($user) || !Hash::check($request->password, $user->password)) throw new Exception('Invalid login credentials.', 404);
                 $accessToken = $user->createToken('authToken')->plainTextToken;
                 // $permissionNames  = $user->getAllPermissions();
                 $permissionNames = $user->getAllPermissions()->pluck('name');
-                $permission=['user'=>$permissionNames];
-                return response()->json([$accessToken, $user, $permission]);
-            
+                // $permission=['user'=>$permissionNames];
+                return response()->json([$user,$accessToken, $permissionNames]);
+            }elseif ($request->type == 'admin') {
+                $admin = Admin::where('email',$request->email)->first();
+                if (empty($admin) || !Hash::check($request->password, $admin->password)) throw new Exception('Invalid login credentials.', 404);
+                $accessToken = $admin->createToken('authToken')->plainTextToken;
+                return response()->json([$admin,$accessToken]);
+            }else{
+                return response()->json(['Type error' => 'Invalid login type'], 400);
+            }
         }catch(QueryException $e){
-            return response()->json(['DB error' => $e->getMessage()], 400);
+            return response()->json(['DB error' => $e->getMessage()], $e->getCode() ?:400);
 
         }catch(Exception $e){
-            return response()->json(['error' => $e->getMessage()], 400);
+            return response()->json(['error' => $e->getMessage()], $e->getCode() ?: 400);
         }
     }
 }
