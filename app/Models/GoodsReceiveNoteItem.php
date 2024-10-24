7e9ce9547748643ce7e65fd460ae36d29bcef5b6
990Huzaifa <syedhuzaifaali366@gmail.com>
2024-10-24 20:00:39 +0500

    minor

--- app/Models/GoodsReceiveNoteItem.php
+++ app/Models/GoodsReceiveNoteItem.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class GoodReceiveNote extends Model
+class GoodsReceiveNoteItem extends Model
 {
     use HasFactory;
 }
