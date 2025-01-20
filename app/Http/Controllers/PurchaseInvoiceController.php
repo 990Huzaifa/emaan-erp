3638386046216b4d43312a9b065314a8c113599c
990Huzaifa <syedhuzaifaali366@gmail.com>
2025-01-20 16:35:21 +0500

    minor

--- app/Http/Controllers/PurchaseInvoiceController.php
+++ app/Http/Controllers/PurchaseInvoiceController.php
@@ -14,7 +14,7 @@ use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Validator;
-use Barryvdh\DomPDF\Facade\Pdf as PDF;
+use Barryvdh\DomPDF\Facade as PDF;
 
 class PurchaseInvoiceController extends Controller
 {
