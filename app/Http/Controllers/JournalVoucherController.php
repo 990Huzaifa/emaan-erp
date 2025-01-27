9c8c1f68b9c07dc0c8fd6cfde755998c002008fe
990Huzaifa <syedhuzaifaali366@gmail.com>
2025-01-27 15:17:04 +0500

    init journal voucher

---
+++ app/Http/Controllers/JournalVoucherController.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\JournalVoucher;
+use Exception;
+use App\Models\Transaction;
+use Illuminate\Database\QueryException;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
+
+class JournalVoucherController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index(Request $request): JsonResponse
+    {
+        try{
+            $user = Auth::user();
+            if ($user->role == 'user') {
+                $businessId = $user->login_business;
+                if (!$user->hasBusinessPermission($businessId, 'list goods received notes')) {
+                    return response()->json([
+                        'error' => 'User does not have the required permission.'
+                    ], 403);
+                }
+            }
+            $perPage = $request->query('per_page', 10);
+            $searchQuery = $request->query('search');
+            $query = JournalVoucher::select('journal_vouchers.*','chart_of_accounts.name as asset_name','uses.name as partner_name')
+            ->join('chart_of_accounts', 'journal_vouchers.acc_id', '=', 'chart_of_accounts.id')
+            ->join('users', 'journal_vouchers.partner_id', '=', 'users.id')
+            ->orderBy('id', 'desc');
+            if (!empty($searchQuery)) {
+                $query = $query->where('order_code', 'like', '%' . $searchQuery . '%');
+            }
+            // Execute the query with pagination
+            $data = $query->paginate($perPage);
+            return response()->json($data,200);
+
+        }catch(QueryException $e){
+            return response()->json(['DB error' => $e->getMessage()], 400);
+        }catch(Exception $e){
+            return response()->json(['error' => $e->getMessage()], 400);
+        }
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
---
+++ app/Models/JournalVoucher.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class JournalVoucher extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'voucher_code',
+        'partner_id',
+        'acc_id',
+        'voucher_amount',
+        'status',
+        'cheque_no',
+        'cheque_date',
+        'payment_method',
+        'business_id',
+    ];
+}
