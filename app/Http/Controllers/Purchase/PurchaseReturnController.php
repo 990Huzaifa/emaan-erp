5d8d44cf51f7b19aae70cd537361d0fb85c72edc
990Huzaifa <syedhuzaifaali366@gmail.com>
2025-03-03 12:39:46 +0500

    minor

--- app/Http/Controllers/Purchase/PurchaseReturnController.php
+++ app/Http/Controllers/Purchase/PurchaseReturnController.php
@@ -202,7 +202,7 @@ class PurchaseReturnController extends Controller
             ]);
             if($data->status == 1){
                 foreach ($data->items as $item) {
-                    $inventory_detail = InventoryDetail::find($item->lot_id);
+                    $inventory_detail = InventoryDetail::where('lot_id',$item->lot_id)->first();
                     $lot = Lot::find($item->lot_id);
                     $inventory_detail->update([
                         'stock' => $inventory_detail->stock - $item->quantity,
