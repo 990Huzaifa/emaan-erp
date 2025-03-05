84aa3474e9e7340d2ba576fc14a24cf1276e8aad
990Huzaifa <syedhuzaifaali366@gmail.com>
2025-03-05 16:17:21 +0500

    init

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,9 +2,57 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\InventoryDetail;
+use Exception;
+use Illuminate\Database\QueryException;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
-    
+    public function nonPaidCustomer()
+    {
+
+    }
+
+    public function inventoryProducts(): JsonResponse
+    {
+        try{
+            $outOfStockProducts = InventoryDetail::select('inventory_details.*','products.name','products.image')
+            ->where('inventory_details.in_stock', 0)
+            ->join('products','inventory_details.product_id','=','products.id')->get();
+            $inStockProducts = InventoryDetail::select('inventory_details.*','products.name','products.image')
+            ->where('inventory_details.in_stock', 1)
+            ->join('products','inventory_details.product_id','=','products.id')->get();
+            $lowInStockProducts = InventoryDetail::select('inventory_details.*','products.name','products.image')
+            ->where('inventory_details.in_stock', 2)
+            ->join('products','inventory_details.product_id','=','products.id')->get();
+
+            $data = [
+                'outOfStockProducts' => $outOfStockProducts,
+                'inStockProducts' => $inStockProducts,
+                'lowInStockProducts' => $lowInStockProducts
+            ];
+
+            return response()->json($data);
+        }catch(QueryException $e){
+            return response()->json(['DB error' => $e->getMessage()], 400);
+        }
+        catch(Exception $e){
+            return response()->json(['error' => $e->getMessage()], 400);
+        }
+    }
+
+    public function salesAnalysis(Request $request): JsonResponse
+    {
+        try{
+            
+            return response()->json();
+        }catch(QueryException $e){
+            return response()->json(['DB error' => $e->getMessage()], 400);
+        }
+        catch(Exception $e){
+            return response()->json(['error' => $e->getMessage()], 400);
+        }
+    }
 }
