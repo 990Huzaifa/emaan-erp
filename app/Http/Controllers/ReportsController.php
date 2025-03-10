f40905eb517fa4377a24103a39f9a7e364f1803b
990Huzaifa <syedhuzaifaali366@gmail.com>
2025-03-10 15:59:49 +0500

    minor

--- app/Http/Controllers/ReportsController.php
+++ app/Http/Controllers/ReportsController.php
@@ -386,7 +386,7 @@ class ReportsController extends Controller
 
             // get inventory
             $grn_ids = GoodsReceiveNote::where('business_id', $businessId)->pluck('id');
-            $products_ids = Lot::whereIn('grn_id', $grn_ids)->distinct('product_id')->pluck('procudt_id');
+            $products_ids = Lot::whereIn('grn_id', $grn_ids)->distinct('product_id')->pluck('product_id');
             $inventory = InventoryDetail::whereIn('product_id', $products_ids)->get();
 
             // getting balance of accounts and total
