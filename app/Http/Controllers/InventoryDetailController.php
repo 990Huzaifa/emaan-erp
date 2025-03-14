c26cc7e762beaba9f86a40d3c5629f8c2afc1b4e
990Huzaifa <syedhuzaifaali366@gmail.com>
2025-03-14 10:20:08 +0500

    add unit in inventory_detail

--- app/Http/Controllers/InventoryDetailController.php
+++ app/Http/Controllers/InventoryDetailController.php
@@ -112,6 +112,8 @@ class InventoryDetailController extends Controller
 
             // Fetch product details (common for all lots)
             $productDetail = Product::select('id', 'title', 'image')
+            ->join('measurement_units', 'products.measurement_unit_id', '=', 'measurement_units.id')
+                ->select('products.id', 'products.title', 'products.image', 'measurement_units.name as measurement_unit')
                 ->where('id', $id)
                 ->first();
 
