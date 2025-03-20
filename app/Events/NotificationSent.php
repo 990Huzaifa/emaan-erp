b10b6ccd13a65629d201b223073c94200bc8ceb5
990Huzaifa <syedhuzaifaali366@gmail.com>
2025-03-20 13:43:32 +0500

    minor

--- app/Events/NotificationSent.php
+++ app/Events/NotificationSent.php
@@ -31,7 +31,7 @@ class NotificationSent implements ShouldBroadcast
 
     public function broadcastOn()
     {
-        return new PrivateChannel('notification.'.$this->userId);
+        return new Channel('notification.'.$this->userId);
     }
 
     public function broadcastWith()
