7fb7c58a2c5c935a3fde529e1a188219e6bfa626
990Huzaifa <syedhuzaifaali366@gmail.com>
2025-03-20 14:21:38 +0500

    minor

--- app/Notifications/GeneralNotification.php
+++ app/Notifications/GeneralNotification.php
@@ -10,7 +10,7 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Notifications\Messages\BroadcastMessage;
 
-class GeneralNotification extends Notification implements ShouldQueue, ShouldBroadcast
+class GeneralNotification extends Notification implements ShouldQueue
 {
     use Queueable;
 
