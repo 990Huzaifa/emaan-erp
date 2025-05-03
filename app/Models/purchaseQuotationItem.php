a290ea7c787cf5f7599760cef10029e11ffdeec8
syed-huzaifa-ali <106827131+990Huzaifa@users.noreply.github.com>
2025-05-03 11:13:10 +0500

    minor

--- app/Models/purchaseQuotationItem.php
+++ app/Models/purchaseQuotationItem.php
@@ -12,7 +12,6 @@ class PurchaseQuotationItem extends Model
     protected $fillable = [
         'purchase_quotation_id',
         'product_id',
-        'measurement_unit',
         'quantity'
     ];
 
