39f82a9e9778a037572b1e7534ddc4bd7ca45ab5
990Huzaifa <syedhuzaifaali366@gmail.com>
2025-05-19 14:07:11 +0500

    minor

--- app/Models/User.php
+++ app/Models/User.php
@@ -28,6 +28,7 @@ class User extends Authenticatable
         'password',
         'setup_code',
         'address',
+        'phone',
         'setup_code_expiry',
         'cnic_images',
         'avatar',
